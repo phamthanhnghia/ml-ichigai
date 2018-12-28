24eb22dd87a20ab4f022eae000bc20d045afbe91
Pham Thanh Nghia <phamthanhnghia731997@gmail.com>
2018-12-28 08:24:13 +0700

    up date

--- basic/views/site/main.php
+++ basic/views/site/main.php
@@ -1,1 +1,16 @@
+<?php
+use Phpml\Classification\SVC;
+use Phpml\SupportVectorMachine\Kernel;
 
+$classifier = new SVC(Kernel::LINEAR, $cost = 1000);
+$classifier = new SVC(Kernel::RBF, $cost = 1000, $degree = 3, $gamma = 6);
+
+$samples = [[4, 4], [1, 4], [2, 4], [3, 1], [4, 1], [4, 2]];
+$labels = ['a', 'a', 'a', 'b', 'b', 'b'];
+
+$classifier = new SVC(Kernel::LINEAR, $cost = 1000);
+$classifier->train($samples, $labels);
+echo '<pre>';
+print_r($classifier->predict([4, 4]));
+echo '</pre>';
+die;
